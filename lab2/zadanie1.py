9022257f914747344cc0173420833bb315a7f4c1
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-08 17:23:47 +0300

    update lab2 directory update zadanie1.py

--- lab2/zadanie1.py
+++ lab2/zadanie1.py
@@ -2,6 +2,14 @@
 # которые упорядочены (либо по возрастанию, либо по убыванию).
 import numpy as np
 
+
+def print_matrix(arr_2d):
+    for arr in arr_2d:
+        for el in arr:
+            print(el, end=' ')
+        print()
+
+
 с = 0
 pr = False
 p = int(input('Введите количество строк в матрице:'))
@@ -12,17 +20,21 @@ for i in range(p):
     for j in range(h):
         a[i, j] = int(input("Введите число:"))
 print('Полученная матрица:')
-print(a)
+print_matrix(a)
 for i in range(p):
     for j in range(h - 1):
         if a[i, j] <= a[i, j + 1]:
             pr = True
-            if pr == True:
-                if (a[i, j] > maxim):
-                    maxim = a[i, j]
+        else:
+            pr = False
+        if pr:
+            if a[i, j] <= maxim:
+                continue
+            maxim = a[i, j]
         else:
             pr = False
             maxim = 0
+            continue
 if maxim == 0:
     print('Упорядоченных строк не найдено')
 else:
