b9694ce28188a0355bec90a6711add6f6f135efd
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-08 20:37:56 +0300

    update lab2 directory update zadanie2.py

--- lab2/zadanie2.py
+++ lab2/zadanie2.py
@@ -9,28 +9,45 @@ def print_matrix(arr_2d):
         print()
 
 
-pr = False
+def mirror_matrix(arr_2d):
+    rows_cnt = len(D)
+    cols_cnt = len(D[0])
+
+    new_matrix = [[0] * rows_cnt for _ in range(cols_cnt)]
+
+    for i in range(rows_cnt):
+        for j in range(cols_cnt):
+            new_matrix[j][i] = D[i][j]
+    return new_matrix
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimum = i
+
+        for j in range(i + 1, len(arr)):
+            # Выбор наименьшего значения
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        # Помещаем это перед отсортированным концом массива
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+
+    return arr
+
+a = []
 k = 0
 j = 1
-pr = False
 m = int(input('Введите количество строк в матрице:'))
 n = int(input('Введите количество столбцов в матрице:'))
 D = np.zeros((m, n))
-maxim = 0
 for i in range(m):
     for j in range(n):
         D[i, j] = int(input("Введите число:"))
 print('Полученная матрица:')
 print_matrix(D)
 k = int(input("введите номер строки, элементы которой нужно расположить в порядке возрастания:"))
-if (1 <= k) and (k <= m):
-    pr = True
-if pr:
-    rows_cnt = len(D)
-    cols_cnt = len(D[0])
+arr = D[k-1]
+arr2 = selection_sort(arr)
+for i in range(arr2):
 
-    new_matix = [[0] * rows_cnt for _ in range(cols_cnt)]
-
-    for i in range(rows_cnt):
-        for j in range(cols_cnt):
-            new_matix[j][i] = D[i][j]
