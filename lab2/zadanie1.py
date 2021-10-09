f35027f94cb2c0ef1d962cb032e4e43b87cf086a
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-09 19:52:35 +0300

    update lab2 directory update zadanie1.py update zadanie2.py

--- lab2/zadanie1.py
+++ lab2/zadanie1.py
@@ -1,39 +1,48 @@
 # Найти максимальный среди всех элементов тех строк заданной матрицы,
 # которые упорядочены (либо по возрастанию, либо по убыванию).
-import numpy as np
-
-
 def print_matrix(arr_2d):
     for arr in arr_2d:
         for el in arr:
             print(el, end=' ')
         print()
 
-j=1
-pr = False
-p = int(input('Введите количество строк в матрице:'))
-h = int(input('Введите количество столбцов в матрице:'))
-a = np.zeros((p, h))
-maxim = a[0, 0]
-for i in range(p):
-    for j in range(h):
-        a[i, j] = int(input("Введите число:"))
-print('Полученная матрица:')
-print_matrix(a)
-for i in range(p):
-    for j in range(h):
-        if a[i, j-1] <= a[i, j ]:
-            pr = True
-        else:
-            pr = False
-    if pr:
-        if a[i, j] <= maxim:
-            continue
-        maxim = a[i, j]
+
+matrix = []
+need_stop = False
+
+while not need_stop:
+    s = input()
+    if s != '':
+        split = s.split(' ')
+        tmp = []
+        for j in split:
+            tmp.append(int(j))
+        matrix.append(tmp)
     else:
-        pr = False
-        maxim = 0
-if maxim == 0:
-    print('Упорядоченных строк не найдено')
-else:
-    print('Наибольшее значение:', maxim)
+        need_stop = True
+
+print('Полученная матрица:')
+print_matrix(matrix)
+sorted_matrix = []
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0]) - 1):
+        if matrix[i][j] < matrix[i][j + 1]:
+            if j == (len(matrix[0]) - 2):
+                sorted_matrix.append(matrix[i])
+        else:
+            break
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0]) - 1):
+        if matrix[i][j] > matrix[i][j + 1]:
+            if j == (len(matrix[0]) - 2):
+                sorted_matrix.append(matrix[i])
+        else:
+            break
+maximum = -99999
+for i in range(len(sorted_matrix)):
+    for j in range(len(sorted_matrix[0])):
+        if sorted_matrix[i][j] > maximum:
+            maximum = sorted_matrix[i][j]
+print('Максимальное значение: ', maximum)
--- lab2/zadanie2.py
+++ lab2/zadanie2.py
@@ -1,76 +1,20 @@
-# Расположить столбцы матрицы D[M, N] в порядке возрастания элементов k-й строки (1 <= k <= М).
-import numpy as np
+matrix = []
+need_stop = False
 
+while not need_stop:
+    s = input()
+    if s != '':
+        split = s.split(' ')
+        tmp = []
+        for j in split:
+            tmp.append(int(j))
+        matrix.append(tmp)
+    else:
+        need_stop = True
 
-def print_matrix(arr_2d):
-    for arr25 in arr_2d:
-        for el in arr25:
-            print(el, end=' ')
-        print()
+k1 = int(input('Введите значение k:'))
+k = k1 - 1
+a = []
+for num in matrix[k]:
+    a.append(num)
 
-
-def mirror_matrix(arr_3d) -> object:
-    rows_cnt = len(arr_3d)
-    cols_cnt = len(arr_3d[0])
-
-    new_matrix = [[0] * rows_cnt for _ in range(cols_cnt)]
-
-    for i in range(rows_cnt):
-        for j in range(cols_cnt):
-            new_matrix[j][i] = arr_3d[i][j]
-    return new_matrix
-
-
-def selection_sort(arr24):
-    tmp = arr24[:]
-    for i in range(len(tmp)):
-        minimum = i
-
-        for j in range(i + 1, len(tmp)):
-            # Выбор наименьшего значения
-            if tmp[j] < tmp[minimum]:
-                minimum = j
-
-        # Помещаем это перед отсортированным концом массива
-        tmp[minimum], tmp[i] = tmp[i], tmp[minimum]
-
-    return tmp
-
-
-a = 0
-k = 0
-j = 1
-m = int(input('Введите количество строк в матрице:'))
-n = int(input('Введите количество столбцов в матрице:'))
-D = np.zeros((m, n))
-tnp = np.zeros((m, n))
-for i in range(m):
-    for j in range(n):
-        a = int(input("Введите число:"))
-        D[i, j] = a
-        tnp[i, j] = a
-print('Полученная матрица:')
-print_matrix(D)
-k = int(input("введите номер строки, элементы которой нужно расположить в порядке возрастания:"))
-ind = []
-arr = tnp[k - 1]
-arr2 = selection_sort(D[k - 1])
-print('arr=', arr)
-print('arr2=', arr2)
-for i in range(len(arr)):
-    for j in range(len(arr2)):
-        if arr[i] == arr2[j]:
-            ind.append(j)
-            break
-print("ind=", ind)
-rev = mirror_matrix(tnp)
-print('rev=')
-print_matrix(rev)
-t = []
-for i in range(n):
-    t.append(rev[ind[i]])
-print('t= ')
-print_matrix(t)
-B = mirror_matrix(t)
-print('b= ')
-print_matrix(B)
