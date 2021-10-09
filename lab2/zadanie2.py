f951f5d433f3f17b15c63d33b64130f60c72a35d
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-09 17:40:18 +0300

    update lab2 directory update zadanie2.py

--- lab2/zadanie2.py
+++ lab2/zadanie2.py
@@ -3,51 +3,81 @@ import numpy as np
 
 
 def print_matrix(arr_2d):
-    for arr in arr_2d:
-        for el in arr:
+    for arr25 in arr_2d:
+        for el in arr25:
             print(el, end=' ')
         print()
 
 
-def mirror_matrix(arr_2d):
-    rows_cnt = len(D)
-    cols_cnt = len(D[0])
+def mirror_matrix(arr_3d) -> object:
+    rows_cnt = len(arr_3d)
+    cols_cnt = len(arr_3d[0])
 
     new_matrix = [[0] * rows_cnt for _ in range(cols_cnt)]
 
     for i in range(rows_cnt):
         for j in range(cols_cnt):
-            new_matrix[j][i] = D[i][j]
+            new_matrix[j][i] = arr_3d[i][j]
     return new_matrix
 
 
-def selection_sort(arr):
-    for i in range(len(arr)):
+def selection_sort(arr24):
+    tmp = arr24[:]
+    for i in range(len(tmp)):
         minimum = i
 
-        for j in range(i + 1, len(arr)):
+        for j in range(i + 1, len(tmp)):
             # Выбор наименьшего значения
-            if arr[j] < arr[minimum]:
+            if tmp[j] < tmp[minimum]:
                 minimum = j
 
         # Помещаем это перед отсортированным концом массива
-        arr[minimum], arr[i] = arr[i], arr[minimum]
+        tmp[minimum], tmp[i] = tmp[i], tmp[minimum]
 
-    return arr
+    return tmp
 
-a = []
+
+a = 0
 k = 0
 j = 1
 m = int(input('Введите количество строк в матрице:'))
 n = int(input('Введите количество столбцов в матрице:'))
 D = np.zeros((m, n))
+tnp = np.zeros((m, n))
 for i in range(m):
     for j in range(n):
-        D[i, j] = int(input("Введите число:"))
+        a = int(input("Введите число:"))
+        D[i,j] = a
+        tnp[i,j] = a
 print('Полученная матрица:')
 print_matrix(D)
 k = int(input("введите номер строки, элементы которой нужно расположить в порядке возрастания:"))
-arr = D[k-1]
-arr2 = selection_sort(arr)
-for i in range(arr2):
-
+ind = []
+arr = tnp[k - 1]
+arr2 = selection_sort(D[k - 1])
+print('arr=', arr)
+print('arr2=', arr2)
+for i in range(len(arr)):
+    for j in range(len(arr2)):
+        if arr[i] == arr2[j]:
+            ind.append(j)
+            break
+print("ind=", ind)
+rev = mirror_matrix(tnp)
+print_matrix(rev)
+t = []
+for i in range(n):
+    t.append(rev[ind[i]])
+B = mirror_matrix(t)
+print_matrix(B)
+# for i in range(n):
+#     c = t[i]
+#     t[i] = t[ind[i]]
+#     t[ind[i]] = c
+# print_matrix(t)
+# for i in range(m):
+#     for j in range(n - 1):
+#         while all(arr) != all(arr2):
+#             D[i, j] = D[i, j + 1]
+#             arr = D[k - 1]
+# print_matrix(D)
