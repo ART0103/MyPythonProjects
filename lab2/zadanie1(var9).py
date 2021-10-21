b741c2ae62c9373b7cdab152072cb0ae7bf3d833
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-21 18:09:55 +0300

    update lab2 directory update zadanie1(var9).py update zadanie2(var9).py

--- lab2/zadanie1(var9).py
+++ lab2/zadanie1(var9).py
@@ -13,18 +13,21 @@ while not need_stop:
     else:
         need_stop = True
 
-k = int(input('Введите число для определения нужных элементов: '))
 sorted_matrix = []
 counter = 0
 
 if len(matrix) == len(matrix[0]):
+    k = int(input('Введите число для определения нужных элементов: '))
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] % k == 0:
                 counter += 1
                 sorted_matrix.append(matrix[i][j])
 
-maxim = sorted_matrix[0]
-for i in range(len(sorted_matrix)):
-    maxim = sorted_matrix[i] if sorted_matrix[i] > maxim else maxim
-print('Наибольшее значение:', maxim, 'Число элементов кратных k:', counter)
+    maxim = sorted_matrix[0]
+    for i in range(len(sorted_matrix)):
+        maxim = sorted_matrix[i] if sorted_matrix[i] > maxim else maxim
+    print('Наибольшее значение:', maxim, 'Число элементов кратных k:', counter)
+else:
+    print('Вы ввели не квадратную матрицу!')
+
--- lab2/zadanie2(var9).py
+++ lab2/zadanie2(var9).py
@@ -1,3 +1,32 @@
 # В данной действительной квадратной матрице порядка n найти наибольший по модулю элемент.
 # Получить квадратную матрицу порядка n — 1 путем отбрасывания из исходной матрицы строки и столбца, на пересечении
 # которых расположен элемент с найденным значением.
+matrix = []
+need_stop = False
+
+while not need_stop:
+    s = input()
+    if s != '':
+        split = s.split(' ')
+        tmp = []
+        for j in split:
+            tmp.append(float(j))
+        matrix.append(tmp)
+    else:
+        need_stop = True
+
+maxim = matrix[0][0]
+column = 0
+row = 0
+
+if len(matrix) == len(matrix[0]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if abs(matrix[i][j]) > maxim:
+                maxim = abs(matrix[i][j])
+                row = i
+                column = j
+    for i in range(len(matrix)):
+        del matrix[i][column]
+    del matrix[row]
+print(matrix)
