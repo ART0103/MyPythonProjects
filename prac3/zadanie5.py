ecd5a5d01c52010a0eb938db3ef9da4be399d9d4
ART0103 <rkbvjd.cfif@mail.ru>
2021-10-29 17:18:18 +0300

    update prac directory add prac3 directiory update zadanie5.py

--- prac3/zadanie5.py
+++ prac3/zadanie5.py
@@ -3,8 +3,10 @@
 print('Введите произвольные отрицательные и положительные числа:')
 array = list(map(int, input().split()))
 max1 = array[0]
-max2 = array[0]
+max2 = array[1]
 for i in range(len(array)):
     if array[i] > max1:
         max1, max2 = array[i], max1
+    elif (array[i] > max2) and (array[i] != max1):
+        max2 = array[i]
 print('Максимальные элементы массива:', max1, max2)
